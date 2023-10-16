708664937ebe6a9182ab0cf5e927e23c3f723a99
DiegoG1117 <130846543+DiegoG1117@users.noreply.github.com>
2023-10-16 18:45:51 -0500

    scripts

--- Assets/Scripts/MovimientoEnemigo.cs
+++ Assets/Scripts/MovimientoEnemigo.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class MovimientoEnemigo : MonoBehaviour
 {
     public Transform objetivo; // El primer objetivo hacia el que se dirige
-    public Transform segundoObjetivo; // El segundo objetivo al que se dirigirá después de la colisión
+    public Transform segundoObjetivo; // El segundo objetivo al que se dirigirá después de la colisión con el primer objetivo
+    public Transform tercerObjetivo; // El tercer objetivo al que se dirigirá después de la colisión con el segundo objetivo
     public float velocidad = 5f; // La velocidad de movimiento
     public float distanciaSuelo = 0.1f; // Distancia mínima desde el suelo
-    public LayerMask capaSuelo; // Capa de suelo
-
     public GameObject enemigo;
 
+
     private Rigidbody2D rb;
-    private bool cambiaDireccion = false;
+    private int direccion = 1;
 
     void Start()
     {
@@ -22,13 +22,14 @@ public class MovimientoEnemigo : MonoBehaviour
 
     void Update()
     {
-        if (!cambiaDireccion)
+        Debug.Log(direccion);
+        if (direccion == 1)
         {
             // Calcula la dirección hacia el primer objetivo
             Vector2 direccion = (objetivo.position - transform.position).normalized;
 
             // Lanza un rayo hacia abajo para detectar la posición del suelo
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, distanciaSuelo, capaSuelo);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, distanciaSuelo);
 
             // Calcula la nueva posición del jugador ajustando la altura
             Vector2 nuevaPosicion = transform.position + new Vector3(direccion.x, direccion.y * hit.distance, 0) * velocidad * Time.deltaTime;
@@ -36,12 +37,27 @@ public class MovimientoEnemigo : MonoBehaviour
             // Aplica la nueva posición
             rb.MovePosition(nuevaPosicion);
         }
-        else
+        if(direccion == 2)
         {
-            // Calcula la dirección hacia el segundo objetivo
+            // Calcula la dirección hacia el siguiente objetivo
             Vector2 direccion = (segundoObjetivo.position - transform.position).normalized;
 
-            // Mueve el personaje hacia el segundo objetivo
+            // Lanza un rayo hacia abajo para detectar la posición del suelo
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, distanciaSuelo);
+
+            // Mueve el personaje hacia el siguiente objetivo
+            Vector2 nuevaPosicion = (Vector2)transform.position + direccion * velocidad * Time.deltaTime;
+            rb.MovePosition(nuevaPosicion);
+        }
+         if(direccion == 3)
+        {
+            // Calcula la dirección hacia el siguiente objetivo
+            Vector2 direccion = (tercerObjetivo.position - transform.position).normalized;
+
+            // Lanza un rayo hacia abajo para detectar la posición del suelo
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, distanciaSuelo);
+
+            // Mueve el personaje hacia el siguiente objetivo
             Vector2 nuevaPosicion = (Vector2)transform.position + direccion * velocidad * Time.deltaTime;
             rb.MovePosition(nuevaPosicion);
         }
@@ -62,7 +78,22 @@ public class MovimientoEnemigo : MonoBehaviour
             // Cambia el objetivo al segundo objetivo
             objetivo = segundoObjetivo;
 
-            cambiaDireccion = true;
+           direccion = 2;
+        }
+        if (collision.transform == segundoObjetivo)
+        {
+            // Cambia el Layer del personaje al Layer "Enemigo3Nivel"
+            enemigo.layer = LayerMask.NameToLayer("Enemigo3Nivel");
+
+            // Cambia la dirección en el eje X por 0 grados (dirección original)
+            Vector3 nuevaRotacion = transform.eulerAngles;
+            nuevaRotacion.y = 0f;
+            transform.eulerAngles = nuevaRotacion;
+
+            // Cambia el objetivo al tercer objetivo
+            objetivo = tercerObjetivo; // Cambia objetivo al tercer objetivo solo si colisiona con el segundo objetivo
+
+            direccion = 3; 
         }
     }
 }
