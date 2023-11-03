05af5dc5797e02ecf34823da57bf1023c3356b0c
DiegoG1117 <130846543+DiegoG1117@users.noreply.github.com>
2023-11-02 22:15:52 -0500

    agregando temporizador

--- Assets/Scripts/CambioTemporal.cs
+++ Assets/Scripts/CambioTemporal.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; // Asegúrate de importar el espacio de nombres TextMeshPro
 
 public class CambioTemporal : MonoBehaviour
 {
     public GameObject Amedieval;
     public GameObject Aguerra;
     public GameObject Afuturista;
-    int numeroAleatorio = 1;
+    public TextMeshProUGUI tiempoText; // Asigna el objeto TextMeshPro en el Inspector
+
+    private float tiempoRestante = 300.0f; // 5 minutos en segundos
 
     // Start is called before the first frame update
     void Start()
@@ -15,24 +18,46 @@ public class CambioTemporal : MonoBehaviour
         Amedieval.SetActive(true);
         Aguerra.SetActive(false);
         Afuturista.SetActive(false);
-
+        
         // Iniciar la corutina para generar un número aleatorio después de esperar 10 segundos
         StartCoroutine(GenerarNumeroDespuesDeEsperar());
+
+        // Actualizar el TextMeshPro con el tiempo restante
+        ActualizarTiempoText();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // La lógica para cambiar entre los objetos debe estar en GenerarNumeroDespuesDeEsperar
+        // Actualizar el tiempo restante
+        tiempoRestante -= Time.deltaTime;
+
+        // Verificar si el tiempo ha llegado a cero
+        if (tiempoRestante <= 0.0f)
+        {
+            tiempoRestante = 0.0f;
+            // Aquí puedes realizar alguna acción una vez que el tiempo llegue a cero
+        }
+
+        // Actualizar el TextMeshPro con el tiempo restante
+        ActualizarTiempoText();
+    }
+
+    void ActualizarTiempoText()
+    {
+        int minutos = (int)(tiempoRestante / 60);
+        int segundos = (int)(tiempoRestante % 60);
+
+        // Formatear el tiempo restante como un string y mostrarlo en el TextMeshPro
+        tiempoText.text = string.Format("{0:00}:{1:00}", minutos, segundos);
     }
 
     IEnumerator GenerarNumeroDespuesDeEsperar()
     {
-        // Esperar 10 segundos antes de generar un número aleatorio
-        yield return new WaitForSeconds(10f);
+        // Esperar 30 segundos antes de generar un número aleatorio
+        yield return new WaitForSeconds(30f);
 
         // Generar un número aleatorio del 1 al 3
-        numeroAleatorio = Random.Range(1, 4);
+        int numeroAleatorio = Random.Range(1, 4);
         Debug.Log("Número aleatorio: " + numeroAleatorio);
 
         // Lógica para cambiar entre los objetos según el número aleatorio
